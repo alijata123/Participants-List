645be1460337e2fa1245be6acc46326697405c27
Ali Jata <alijataaa@gmail.com>
2017-05-16 00:03:20 +0300

    server.js changed

--- server.js
+++ server.js
@@ -10,28 +10,7 @@ new WebpackDevServer(webpack(config), {
 }).listen(3000, 'localhost', function (err, result) {
   if (err) {
     return console.log(err);
-  }
+  };
+
   console.log('Listening at http://localhost:3000/');
-});
-
-
-
-var express = require('express');
-
-// Create our app
-var app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url);
-  }
-});
-
-app.use(express.static('public'));
-
-app.listen(PORT, function () {
-  console.log('Express server is up on port ' + PORT);
 });
