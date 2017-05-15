fbf1310d75deecd55c67a6024ef08fa1f56420d9
Ali Jata <alijataaa@gmail.com>
2017-05-15 23:12:41 +0300

    file server.js changed

--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 var webpack = require('webpack');
-var express = require('express');
+
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
-var port = process.env.PORT || 3000 ;
-
-
-
-
-// Create our app
-var app = express();
-const PORT = process.env.PORT || 3000;
-
-app.use(function (req, res, next){
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
-  } else {
-    res.redirect('http://' + req.hostname + req.url);
+new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true
+}).listen(3000, 'localhost', function (err, result) {
+  if (err) {
+    return console.log(err);
   }
-});
-
-app.use(express.static('public'));
-
-app.listen(PORT, function () {
-  console.log('Express server is up on port ' + PORT);
+  console.log('Listening at http://localhost:3000/');
 });
